4edf8634b666317ae7cb92def2830f4e9e5291d2
Por y para que? <92597680+Obsolic@users.noreply.github.com>
2023-06-30 13:14:53 +0200

    add page to change key
    Se ma orvidao

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
 Config.Team = { 'superadmin', 'admin', 'mod' }
-Config.KeyOpen = 38 -- TECLA [E] PERO SE PUEDE CAMBIAR BUSCANDO LA TECLA EN: 
+Config.KeyOpen = 38 -- TECLA [E] PERO SE PUEDE CAMBIAR BUSCANDO LA TECLA EN: https://docs.fivem.net/docs/game-references/controls/
